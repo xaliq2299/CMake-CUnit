d0fc6811ea2ecd1a4be88ae7c45542aa1b79244f
Xaliq <xaliq2299@mail.ru>
2019-04-11 22:39:23 +0400

    Change cmake for new version

---
+++ src/divide.c
@@ -0,0 +1,5 @@
+#include "divide.h"
+
+double divide(double x, double y){
+	return x/y;
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "add.h"
+#include "divide.h"
+
+int main(){
+	printf("%lf\n", add(7,5) );
+	printf("%lf\n", divide(6,2));
+	return 0;
+}
