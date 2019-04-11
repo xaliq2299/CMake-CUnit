2d7bf88ec50ea4bd9d650522d5605e16ac3e1e95
xaliq2299 <47387860+xaliq2299@users.noreply.github.com>
2019-04-11 21:48:26 +0400

    New files

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef __ADD_H_
+#define __ADD_H_
+
+double add(double a, double y);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "add.h"
+
+int main(){
+	printf("%lf\n", add(7,5) );
+	return 0;
+}
